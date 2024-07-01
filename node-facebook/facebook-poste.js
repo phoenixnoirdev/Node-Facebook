bf0b8da33b8a4f19b9004f1acaa1775fb6b75b4b
Phoenixnoir Développement <kevin.philotas.developpement@gmail.com>
2024-07-01 19:49:01 +0200

    01.07.2024  Clean and optimize
    - Correction mineur.
    - Suppression d'un fonction redondante qui provoquait un double poste.
    - Optimisation du code

--- node-facebook/facebook-poste.js
+++ node-facebook/facebook-poste.js
@@ -7,7 +7,7 @@
 //  Contributor:
 //
 //  Created: 2024-06-25 : 11:56:00
-//  Update : 2024-07-01 : 11:30:00
+//  Update : 2024-07-01 : 18:53:00
 //
 //  Description:
 //
@@ -33,7 +33,7 @@ module.exports = function(RED) {
 
             // Construire le chemin complet du dossier JEUX
             const imagePath = config.imagePath || "Dossier images";
-            const jeux = msg.game ||config.game || "JEUX";
+            const jeux = msg.game || config.game || "JEUX";
             const jeuxFolderPath = path.join(imagePath, jeux);
 
             // Vérifier l'existence du dossier JEUX
@@ -46,71 +46,36 @@ module.exports = function(RED) {
                 FB.setAccessToken(accessToken);
 
                 let response;
-                if (jeuxFolderExists) {
-                    // Dossier JEUX existe, utiliser une image spécifique
-                    const filesInJeux = fs.readdirSync(jeuxFolderPath);
-                    if (filesInJeux.length > 0) {
-                        const firstImage = path.join(jeuxFolderPath, filesInJeux[0]);
-                        const fileStream = fs.createReadStream(firstImage);
 
-                        console.log("Dossier + image trouver.");
-                        response = await new Promise((resolve, reject) => {
-                            FB.api(`/${pageId}/photos`, 'POST', {
-                                source: fileStream,
-                                caption: message,
-                                access_token: accessToken
-                            }, (res) => {
-                                if (!res || res.error) {
-                                    reject(res.error || new Error("Erreur inconnue lors de l'upload de l'image"));
-                                } else {
-                                    resolve(res);
-                                }
-                            });
+                if (jeuxFolderExists && fs.readdirSync(jeuxFolderPath).length > 0) {
+                    // Dossier JEUX existe et contient des images
+                    const firstImage = path.join(jeuxFolderPath, fs.readdirSync(jeuxFolderPath)[0]);
+                    const fileStream = fs.createReadStream(firstImage);
+
+                    console.log("Dossier et image trouvée.");
+
+                    // Uploader l'image sur Facebook
+                    response = await new Promise((resolve, reject) => {
+                        FB.api(`/${pageId}/photos`, 'POST', {
+                            source: fileStream,
+                            caption: message,
+                            access_token: accessToken
+                        }, (res) => {
+                            if (!res || res.error) {
+                                reject(res.error || new Error("Erreur inconnue lors de l'upload de l'image"));
+                            } else {
+                                resolve(res);
+                            }
                         });
+                    });
 
-                        // Vérifier si l'upload a réussi
-                        if (!response || !response.id) {
-                            throw new Error("L'upload de l'image a échoué : " + JSON.stringify(response));
-                        }
-
-                        // Publier l'image sur le fil d'actualité
-                        response = await new Promise((resolve, reject) => {
-                            FB.api(`/${pageId}/feed`, 'POST', {
-                                message: message,
-                                object_attachment: response.id,
-                                access_token: accessToken
-                            }, (res) => {
-                                if (!res || res.error) {
-                                    reject(res.error || new Error("Erreur inconnue lors de la publication"));
-                                } else {
-                                    resolve(res);
-                                }
-                            });
-                        });
-                    } else {
-                        console.log("Aucune image trouvée dans le dossier " + JSON.stringify(jeux));
-                        // Dossier JEUX n'existe pas, utiliser l'image par défaut
-                        const fileStream = fs.createReadStream(defaultImagePath);
-
-                        response = await new Promise((resolve, reject) => {
-                            FB.api(`/${pageId}/photos`, 'POST', {
-                                source: fileStream,
-                                caption: message,
-                                access_token: accessToken
-                            }, (res) => {
-                                if (!res || res.error) {
-                                    reject(res.error || new Error("Erreur inconnue lors de l'upload de l'image par défaut"));
-                                } else {
-                                    resolve(res);
-                                }
-                            });
-                        });
-                    }
                 } else {
-                    // Dossier JEUX n'existe pas, utiliser l'image par défaut
-                    console.log("Dossier " + JSON.stringify(jeux) + " n'existe pas, image par défaut utiliser.");
+                    // Dossier JEUX n'existe pas ou est vide, utiliser l'image par défaut
+                    console.log("Aucune image trouvée dans le dossier " + JSON.stringify(jeux));
+
                     const fileStream = fs.createReadStream(defaultImagePath);
 
+                    // Uploader l'image par défaut sur Facebook
                     response = await new Promise((resolve, reject) => {
                         FB.api(`/${pageId}/photos`, 'POST', {
                             source: fileStream,
@@ -124,30 +89,16 @@ module.exports = function(RED) {
                             }
                         });
                     });
+                }
 
-                    // Vérifier si l'upload a réussi
-                    if (!response || !response.id) {
-                        throw new Error("L'upload de l'image par défaut a échoué : " + JSON.stringify(response));
-                    }
-
-                    // Publier l'image par défaut sur le fil d'actualité
-                    response = await new Promise((resolve, reject) => {
-                        FB.api(`/${pageId}/feed`, 'POST', {
-                            message: message,
-                            object_attachment: response.id,
-                            access_token: accessToken
-                        }, (res) => {
-                            if (!res || res.error) {
-                                reject(res.error || new Error("Erreur inconnue lors de la publication de l'image par défaut"));
-                            } else {
-                                resolve(res);
-                            }
-                        });
-                    });
+                // Vérifier si l'upload a réussi
+                if (!response || !response.id) {
+                    throw new Error("L'upload de l'image a échoué : " + JSON.stringify(response));
                 }
 
                 msg.payload = response;
                 node.send(msg);
+
             } catch (error) {
                 if (error && error.message) {
                     if (error.message.includes("Error validating access token")) {
@@ -158,13 +109,7 @@ module.exports = function(RED) {
                     } else {
                         node.error("Erreur : " + error.message, msg);
                     }
-                } else if (error && error.code === 'ECONNRESET') {
-                    // Gérer les erreurs de connexion réinitialisée
-                    node.error("Erreur de connexion réinitialisée : " + error.message, msg);
-                    // Tentative de réessai après un court délai
-                    setTimeout(() => postToFacebook(msg), 1000); // Réessayer après 1 seconde
                 } else {
-                    // Gérer les autres erreurs
                     node.error("Erreur inconnue lors de la publication sur Facebook.", msg);
                 }
             }
